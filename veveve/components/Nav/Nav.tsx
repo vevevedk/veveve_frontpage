1aa0393fc4ffc55ce3d63eded12be3e18edbc976
Michael <hovgaard1990@gmail.com>
2023-01-17 14:46:18 +0100

    Added navigation with constructor and a class to create new links

---
+++ veveve/components/Nav/Nav.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import styles from "../../styles/navbar.module.css";
+import { LinkingModel } from "../model/LinkModel";
+
+interface Props {
+  links: LinkingModel[];
+}
+
+const MobileNav: React.FC<Props> = ({ links }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className={styles.nav}>
+      {/* <img src="path/to/logo" alt="Logo" className={styles.logo} /> */}
+      <ul className={`${styles.links} ${isOpen ? styles.open : ""}`}>
+        {links.map((link, index) => (
+          <li
+            key={link.id}
+            className={index === links.length - 1 ? styles.lastLink : "link"}
+          >
+            <a href={link.idtojump} className={styles.link}>
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <button className={styles.menuButton} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "☰" : "✖"}
+      </button>
+    </nav>
+  );
+};
+
+export default MobileNav;
