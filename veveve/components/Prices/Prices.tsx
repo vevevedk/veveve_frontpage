543e80e2150893d5dc5db0e8660f68cd2e9e8a8a
Michael <hovgaard1990@gmail.com>
2023-01-24 13:27:59 +0100

    added responsive in same component.
    Need styling

--- veveve/components/Prices/Prices.tsx
+++ veveve/components/Prices/Prices.tsx
@@ -5,6 +5,7 @@ import style from "../../styles/priceStyle.module.css";
 import CTA, { stil, tekst } from "../CTA/CTA";
 
 const Prices: React.FC = () => {
+  const [width, setWidth] = useState(window.innerWidth);
   const [price, setPrices] = useState<PriceData[]>([]);
   const [activeTab, setActiveTab] = useState<string>("");
 
@@ -18,55 +19,97 @@ const Prices: React.FC = () => {
       .catch((err) => console.error(err));
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const handleTabClick = (tabTitle: string) => {
     setActiveTab(tabTitle);
   };
 
   return (
     <>
-      <div className={style.priceWrap}>
-        <h2>Beskrivelse af priser</h2>
-        <div className={style.tabBar}>
-          {price.map((prices) => (
-            <button
-              className={`${style.tabButton} ${
-                activeTab === prices.title ? style.activeTabButton : ""
-              }`}
-              key={prices.id}
-              onClick={() => handleTabClick(prices.title)}
-            >
-              <h3 className={style.title}>{prices.title}</h3>
-            </button>
-          ))}
-        </div>
-        <div className={style.content}>
-          {price.map((prices) => (
-            <div
-              key={prices.id}
-              className={activeTab === prices.title ? "" : style.inactiveTab}
-            >
-              <h3> {prices.title}</h3>
-              {prices.servicesIncluded.map((service) => (
-                <ul key={prices.id} className={style.inc}>
-                  <li>
-                    <p> {service}</p>
-                  </li>
-                </ul>
+      {width >= 600 ? (
+        <>
+          <div className={style.priceWrapPC}>
+            <h2>Beskrivelse af priser</h2>
+            <div className={style.content}>
+              {price.map((prices) => (
+                <div key={prices.id} className={style.cards}>
+                  <p className={style.price}>{prices.price} Dkk ex moms</p>
+                  <h3> {prices.title}</h3>
+                  {prices.servicesIncluded.map((service) => (
+                    <ul key={prices.id} className={style.inc}>
+                      <li>
+                        <p> {service}</p>
+                      </li>
+                    </ul>
+                  ))}
+                  <CTA
+                    stil={stil.orange}
+                    tekst={tekst.kontakt}
+                    popup={
+                      <div>
+                        <h3> Oih diz is da shizzle</h3>
+                      </div>
+                    }
+                  />
+                </div>
               ))}
-              <p className={style.price}>{prices.price} Dkk ex moms</p>
             </div>
-          ))}
-          <CTA
-            stil={stil.orange}
-            tekst={tekst.kontakt}
-            popup={
-              <div>
-                <h3> Oih diz is da shizzle</h3>
-              </div>
-            }
-          />
-        </div>
-      </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={style.priceWrap}>
+            <h2>Beskrivelse af priser</h2>
+            <div className={style.tabBar}>
+              {price.map((prices) => (
+                <button
+                  className={`${style.tabButton} ${
+                    activeTab === prices.title ? style.activeTabButton : ""
+                  }`}
+                  key={prices.id}
+                  onClick={() => handleTabClick(prices.title)}
+                >
+                  <h3 className={style.title}>{prices.title}</h3>
+                </button>
+              ))}
+            </div>
+            <div className={style.content}>
+              {price.map((prices) => (
+                <div
+                  key={prices.id}
+                  className={
+                    activeTab === prices.title ? "" : style.inactiveTab
+                  }
+                >
+                  <h3> {prices.title}</h3>
+                  {prices.servicesIncluded.map((service) => (
+                    <ul key={prices.id} className={style.inc}>
+                      <li>
+                        <p> {service}</p>
+                      </li>
+                    </ul>
+                  ))}
+                  <p className={style.price}>{prices.price} Dkk ex moms</p>
+                </div>
+              ))}
+              <CTA
+                stil={stil.orange}
+                tekst={tekst.kontakt}
+                popup={
+                  <div>
+                    <h3> Oih diz is da shizzle</h3>
+                  </div>
+                }
+              />
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
