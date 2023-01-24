b114607a05fbc52ea1a5b15e09ab4256c5975cad
Michael <hovgaard1990@gmail.com>
2023-01-24 09:31:55 +0100

    Bg fixed. added changes in the global CSS so the background chenges on even and on even. The H2 of the class will change with the background.

--- veveve/components/footer/controller/FooterInfo.tsx
+++ veveve/components/footer/controller/FooterInfo.tsx
@@ -10,10 +10,10 @@ interface Footer {
 const FooterInfo: React.FC<Footer> = ({ Line1, Line2, Line3, Line4 }) => {
   return (
     <div className={Style.FooterInfo}>
-      <h2>{Line1}</h2>
-      <h2>{Line2}</h2>
-      <h2>{Line3}</h2>
-      <h2>{Line4}</h2>
+      <h3>{Line1}</h3>
+      <h3>{Line2}</h3>
+      <h3>{Line3}</h3>
+      <h3>{Line4}</h3>
     </div>
   );
 };
